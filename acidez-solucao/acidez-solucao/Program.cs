3866979df8f20dd0dd8527bc1c06879d92700772
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2025-05-16 18:49:46 -0300

    solucao acidez e imc

---
+++ acidez-solucao/acidez-solucao/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace acidez_solucao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Escreva um programa que leia o valor do índice de acidez (pH) de uma solução e informe se ela é ácida, 
+                básica ou neutra, sendo que o pH deve estar entre 0 e 14.
+                A solução é ácida quando o pH é menor que 7
+                A solução é básica quando o pH é maior que 7
+                Caso contrário a solução é neutra
+             */
+
+            double ph;
+
+            Console.WriteLine("Digite o valor do pH da solução: ");
+            ph = Convert.ToDouble(Console.ReadLine());
+
+            if (ph > 7) 
+            {
+                Console.WriteLine("Solução básica");
+            }
+            if (ph == 7)
+            {
+                Console.WriteLine("Solução neutra");
+            }
+            if (ph < 7)
+            {
+                Console.WriteLine("Solução ácida");
+            }
+
+        }
+    }
+}
---
+++ calculo-imc/calculo-imc/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculo_imc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Crie um programa que calcule o IMC de uma pessoa e informe, de acordo com o
+             * valor do IMC se a pessoa está em baixo peso, peso adequado, sobrepeso ou em obesidade
+             */
+
+            double peso;
+            double altura;
+            double imc;
+
+            Console.WriteLine("Digite sua altura (m): ");
+            altura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite seu peso (kg): ");
+            peso = Convert.ToDouble(Console.ReadLine());
+
+            imc = peso / (altura * altura);
+
+            if (imc < 18.5)
+            {
+                Console.WriteLine("Seu IMC é: " + imc);
+                Console.WriteLine("\nBaixo peso");
+            }
+            if (imc >= 18.5 && imc <= 24.9)
+            {
+                Console.WriteLine("Seu IMC é: " + imc);
+                Console.WriteLine("\nPeso adequado");
+            }
+            if (imc > 25 && imc <= 29.9)
+            {
+                Console.WriteLine("Seu IMC é: " + imc);
+                Console.WriteLine("\nSobrepeso");
+            }
+            if (imc > 30 && imc <= 34.9)
+            {
+                Console.WriteLine("Seu IMC é: " + imc);
+                Console.WriteLine("\nObesidade grau I");
+            }
+            if (imc > 35 && imc <= 39.9)
+            {
+                Console.WriteLine("Seu IMC é: " + imc);
+                Console.WriteLine("\nObesidade grau II");
+            }
+            if (imc >= 40)
+            {
+                Console.WriteLine("Seu IMC é: " + imc);
+                Console.WriteLine("\nObesidade grau III");
+            }
+        }
+    }
+}
