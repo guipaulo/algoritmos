727ccae97d6ecae0a71cdcacc427604459373a28
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2025-05-16 09:17:56 -0300

    CSHARP

---
+++ calculadora/calculadora/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {// 
+            double n1, n2;
+            char operacao;
+
+            Console.WriteLine("Menu de opção");
+            Console.WriteLine(" + para operação de soma");
+            Console.WriteLine(" - para operação de subtração ");
+            Console.WriteLine(" * para operação de multiplicação");
+            Console.WriteLine(" / para operação de divisão");
+            Console.WriteLine(" Informe a operação desejada: ");
+
+            operacao = Convert.ToChar(Console.ReadLine());
+
+            switch(operacao)
+            {
+                case '+':
+                    Console.WriteLine("Digite um número: ");
+                    n1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Digite outro número: ");
+                    n2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine(" O resultado da operação é: " + (n1 + n2));
+                    break;
+
+                case '-':
+                    Console.WriteLine("Digite um número: ");
+                    n1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Digite outro número: ");
+                    n2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine(" O resultado da Operação é: " + (n1-n2));
+                    break;
+
+                case '*':
+                    Console.WriteLine("Digite um número: ");
+                    n1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Digite outro número: ");
+                    n2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine(" O resultado da operação é: " + (n1 *  n2));
+                    break;
+
+                case '/':
+                    Console.WriteLine("Digite um número: ");
+                    n1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Digite outro número: ");
+                    n2 = Convert.ToDouble(Console.ReadLine());
+                    if (n2==0)
+                    { 
+                        Console.WriteLine(" Não é possível executar esta operação");
+                    }
+                    else
+                    {
+                        Console.WriteLine("O resultado da operação é:" + (n1 / n2));
+                    }
+                       
+                    break;
+
+            }
+
+        }//
+    }
+}
---
+++ calcular-idade/calcular-idade/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calcular_idade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int anonasc, ano;
+
+            Console.WriteLine("Digite o seu ano de nascimento: ");
+            anonasc = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o ano atual: ");
+            ano = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sua idade é: " + (ano-anonasc));
+
+
+
+
+
+
+        }
+    }
+}
