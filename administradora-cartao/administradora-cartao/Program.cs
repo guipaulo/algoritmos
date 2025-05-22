242c6ae65cd110c51557087bd598ef734f600202
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2025-05-22 20:00:40 -0300

    administradora cartao

---
+++ administradora-cartao/administradora-cartao/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace administradora_cartao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Uma administradora de cartões está oferecendo uma promoção aos seus clientes.
+             *  A pessoa que não puder pagar o total da fatura no mês de março, poderá pagar
+             *  apenas 50% do valor, e o restante poderá ser pago no mês seguinte com juros de 6,5%.
+             *  Desenvolva uma solução para ajudar o cliente a descobrir qual será o valor de sua fatura 
+             *  no mês de abril caso ele aceite a proposta. 
+             */
+
+            double divida = 0;
+            double pagMarco = 0;
+            double pagAbril = 0;
+
+            Console.WriteLine("Informe o valor de sua dívida: ");
+            divida = Convert.ToDouble(Console.ReadLine());
+            pagMarco = divida / 2;
+            pagAbril = pagMarco * Math.Pow((1 + 0.065), 1);
+
+
+            Console.WriteLine("O valor a ser pago em Março é " + pagMarco);
+            Console.WriteLine("O valor a ser pago em Abril é " + pagAbril);
+        }
+    }
+}
---
+++ administradora-cartao/administradora-cartao/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("administradora-cartao")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("administradora-cartao")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("04ac0202-ac41-45b1-aded-e34a72a4855d")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
