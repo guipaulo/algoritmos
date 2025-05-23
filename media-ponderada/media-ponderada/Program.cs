dcc014d7b891ac687e1c77acb7be51be87f859aa
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2025-05-22 21:46:02 -0300

    media ponderada incompleta

---
+++ media-ponderada/media-ponderada/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace media_ponderada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { /*
+           * Escreva um programa que recebe 3 notas de prova e calcula
+           * A média delas
+           * A média ponderada delas, considerando pesos 2, 2 e 3
+           * A média ponderada delas, considerando pesos 1, 2 e 2
+           */
+
+            double num1 = 0, num2 = 0, num3 = 0;
+            double media, pond1, pond2;
+
+            Console.WriteLine("Digite a primeira nota ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a segunda nota ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a terceira nota ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            media = (num1 + num2 + num3) / 3;
+            pond1 = ((num1 * 2) + (num2 * 2) * (num3 * 3)) / 7;
+            pond2 = (num1 * 2) + (num2 * 2) * (num3 * 2) / 5;
+
+            Console.WriteLine("Suas notas são: \n" + pond1);
+
+        }
+    }
+}
---
+++ media-ponderada/media-ponderada/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("media-ponderada")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("media-ponderada")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("f3b50778-9e7d-4e03-a8ca-521500b1b76a")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
