7de8dd8bd4d2223554ed77a4acbf42e59acd9f1b
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2025-05-23 19:21:17 -0300

    atualizacao

---
+++ baskhara/baskhara/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baskhara
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double A = 0;
+            double B = 0;
+            double C = 0;
+
+            A = Convert.ToDouble(Console.ReadLine());
+            B = Convert.ToDouble(Console.ReadLine());
+            C = Convert.ToDouble(Console.ReadLine());
+
+            double delta = (Math.Pow(B, 2) - (4*A*C));
+
+            double x1 = (-B + (Math.Sqrt(delta))/(2 * A));
+            double x2 = (-B - (Math.Sqrt(delta)) /( 2 * A));
+
+            Console.WriteLine("A primeira raíz é "+ x1);
+            Console.WriteLine("A segunda raíz é " + x2);
+        }
+    }
+}
