5b8afde30a4fc044e65447435cd7f86403ce7d89
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2025-06-01 20:12:30 -0300

    10 multiplos

---
+++ 10-multiplos/10-multiplos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_multiplos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            for (int i = 1; i <= 10; i++) 
+            {
+                Console.WriteLine(numero * i);
+            }
+        }
+    }
+}
