8e57e2e03d3777eb2fea58597abe8069cac5ac4c
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2025-06-01 20:41:29 -0300

    maior numero

---
+++ maior-numero/maior-numero/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maior_numero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int maior = 0;
+            int menor = 0;
+
+            Console.WriteLine("Digite os numeros: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                num1 = maior;
+                num2 = menor;
+
+                Console.WriteLine(maior + " " + menor);
+            }
+            else
+            {
+                if (num1 < num2) 
+                {
+                    num1 = menor;
+                    num2 = maior;
+                    Console.WriteLine(maior + " " + menor);
+                }
+            }
+        }
+    }
+}
