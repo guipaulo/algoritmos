5cf5c5336cec35ca3f8cb2192a72d18ffa93e3f0
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2025-06-01 21:03:05 -0300

    ano

---
+++ anos-bisextos/anos-bisextos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anos_bisextos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite dois anos bisextos: ");
+            int ano1 = Convert.ToInt32(Console.ReadLine());
+            int ano2 = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = ano1; i < ano2; i=i+4)
+            {
+                Console.WriteLine(i);
+            }
+
+        }   
+    }
+}
