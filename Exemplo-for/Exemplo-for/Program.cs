159997269a68a390ea708d3f339cc6c36d39d1d5
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2025-06-03 20:35:43 -0300

    novos codigos

---
+++ Exemplo-for/Exemplo-for/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo_for
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int x = 1; x <= 10; x++) 
+            {
+                int resultado = num * x;
+                Console.WriteLine(resultado);
+        }   }
+    }
+}
---
+++ par-impar/par-impar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace par_impar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine("Digite um número");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("Numero par");
+            }
+            else
+            {
+                Console.WriteLine("Número impar");
+            }
+        }
+    }
+}
