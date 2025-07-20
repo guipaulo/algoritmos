a92b9d737dccad63495c758f4160e29ded8a89cb
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2025-07-19 21:27:34 -0300

    matrizes pt 2

---
+++ impressão-triangulo-matriz/impressão-triangulo-matriz/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace impressão_triangulo_matriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] x =  new int[3, 3];
+
+            // Implementando a raiz:
+            for (int i = 0;  i < x.GetLength(0); i++)
+            {
+                for(int j=0; j < x.GetLength(1); j++)
+                {
+                    Console.WriteLine("Digite um número para a posição " + i + ", " + j + ": ");
+                    x[i,j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            //Imprimindo o trianculo superior:
+            Console.WriteLine("Imprimindo o triângulo superior da matriz: ");
+            for (int i = 0; i < x.GetLength(0); i++)
+            {
+                for (int j = 0; j < x.GetLength(1); j++)
+                {
+                    Console.WriteLine(x[i, j]);
+                }
+            }
+
+            //Imprimindo o triangulo inferior
+            Console.WriteLine("Imprimindo o triângulo inferior da matriz: ");
+            for(int i = 0;i < x.GetLength(0); i++) 
+            {
+                for (int j = 0 ; j<= 1; j++)
+                {
+                    Console.WriteLine(x[i, j]);
+                }
+            }
+
+            Console.WriteLine("Imprimindo a diagonal principal da matriz:" );
+            for(int i = 0; i < x.GetLength(0); i++)
+            {
+                Console.WriteLine(x[i, i]);
+            }
+        }
+    }
+}
---
+++ matrizes-2/matrizes-2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matrizes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarando um Array
+            string[] semana = new string[7];
+            string[,] nome = new string[2, 2];
+
+            semana[0] = "Segunda-feira";
+            semana[1] = "Terça-feira";
+            semana[2] = "Quarta-feira";
+            semana[3] = "Quinta=feira";
+            semana[4] = "Sexta-feira";
+            semana[5] = "Sabado";
+            semana[6] = "Domingo";
+
+            Console.WriteLine(semana[6] + "\n");
+
+            for (int i = 0; i < semana.Length; i++)
+            {
+                Console.WriteLine(semana[i] + "\n");
+            }
+
+            Console.WriteLine("Digite um nome: ");
+
+            for (int linha = 0; linha < 2; linha++)
+            {
+                for (int coluna = 0; coluna < 2; coluna++)
+                {
+                    nome[linha, coluna] = Console.ReadLine();
+                }
+                Console.WriteLine();
+            }
+
+            for (int linha = 0; linha <= 1; i++)
+            {
+                for (int coluna = 0; coluna <= 1; j++)
+                {
+                    matriz[linha, coluna] = Convert.ToDouble(Console.ReadLine());
+
+                    if (matriz[linha, coluna] > maior)
+                    {
+                        maior = matriz[linha, coluna];
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
