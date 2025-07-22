e5deabefd54fb40b11786a9865e617ceaf130f3b
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2025-07-22 09:43:38 -0300

    nova impressão de matriz

---
+++ matriz3exercicio/matriz3exercicio/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matriz3exercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = new int[3, 3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine("Digite um número para incluir na matriz: ");
+                    matriz[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0;i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine("Matriz " + "[" + i + "," + j + "] = " + matriz[i, j]);
+                }
+            }
+        }
+    }
+}
