0a99b8d059553fd97c8e4a1ee66427dd56f26301
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2025-07-22 20:56:35 -0300

    matriz2207

---
+++ matrizes2207/matrizes2207/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matrizes2207
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = new int[5,5];
+
+            for(int i = 0; i <=4; i++)
+            {
+                for (int j = 0; j <=4; j++)
+                {
+                    Console.Write("Digite um número na posição " + i + "," + j + ": ");
+                    matriz[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i <=4;i++)
+            {
+                for (int j = 0;j <=4;j++)
+                {
+                    Console.WriteLine("Elemento da posição " + i + j +" é " + matriz[i, j]);
+                }
+            }
+        }
+    }
+}
