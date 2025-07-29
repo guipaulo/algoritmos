98b71980ff0ea96f11a87117be551d3ee4738a6b
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2025-07-29 20:06:02 -0300

    listas csharp

---
+++ listas-csharp/listas-csharp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listas_csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List <int> list = new List<int>();
+
+            // Adicionar elementos na lista
+            list.Add (1);
+            list.Add (2);
+            list.Add (3);
+
+            Console.WriteLine(list[0]);
+            Console.WriteLine(list[1]);
+
+            Console.WriteLine();
+
+            List<int> list2 = new List<int>();
+            list2.Add (2);
+            list2.Add (3);
+            list2.Add(4);
+            list2.Add(5);
+            
+            Console.WriteLine(list2.Count() + " itens"); // Contar os elementos da lista
+            Console.WriteLine(list2.IndexOf(3)); // Retornar a posição de um elemento da lista
+
+            Console.WriteLine();
+
+            List<int> list3 = new List<int>();
+
+            list3.Add (3);
+            list3.Add (1);
+            list3.Add (2);
+            list3.Add (7);
+            list3.Add (5);
+
+            Console.WriteLine(list3[0]);
+            list3.Sort(); // Ordenar elementos da lista
+            Console.WriteLine(list3[0]);
+
+            Console.WriteLine();
+
+            foreach (int i in list3) // Iterar elementos da lista (Ordenados)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ listas-csharp/listas-csharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("listas-csharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("listas-csharp")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("af97cc7a-c4a9-4d50-baf9-fb19e3c649a8")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
