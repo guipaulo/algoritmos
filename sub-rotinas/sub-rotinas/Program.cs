270d00b2e6f96840cdb14f6a91b225d0d36c2ff7
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2025-08-05 22:39:44 -0300

    sub-rotina

---
+++ sub-rotinas/sub-rotinas/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sub_rotinas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sal, aum, novoSal;
+            Console.WriteLine("Digite o salário: ");
+            sal = Convert.ToDouble(Console.ReadLine());
+            aum = Calculo(sal);
+            novoSal = sal + aum;
+            Console.WriteLine("O novo salário é: " + novoSal);
+        }
+
+        static double Calculo(double sal)
+        {
+            double perc, valor;
+            Console.WriteLine("Digite o percentual de aumento: ");
+            perc = Convert.ToDouble(Console.ReadLine());
+            valor = sal * perc / 100;
+
+            return valor;
+
+        }
+    }
+}
---
+++ sub-rotinas/sub-rotinas/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("sub-rotinas")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("sub-rotinas")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("d29c2031-c90d-4a3b-94e3-e445cf2a30dc")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
